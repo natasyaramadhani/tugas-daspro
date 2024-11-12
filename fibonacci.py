23c27d7cbc45afec0bdfe14b2d73541c6c885bf3
natasyaramadhani <natasyaramadhani07@upi.edu>
2024-11-12 20:06:11 +0700

    Update validasi_input on fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -6,9 +6,20 @@ def fibonacci(n):
     a, b = 0, 1
     for i in range(n):
         print(a, end=" ")
-        a, b = b, a+b
-N = int(input("Masukkan nilai N: "))
-if N > 0:
-    fibonacci(N) 
-else:
-    print("Masukkan bilangan positif.")
+        a, b = b, a + b
+    print()
+
+def validasi_input():
+    try:
+        N = int(input("Masukkan nilai N: "))
+        if N > 0:
+            return N
+        else:
+            print("Masukkan bilangan positif.")
+            return None
+    except ValueError:
+        print("Input tidak valid. Harap masukkan angka integer.")
+        return None
+N = validasi_input()
+if N is not None:
+    fibonacci(N)
